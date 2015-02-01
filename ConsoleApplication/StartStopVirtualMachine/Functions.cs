92cd7b5fd82e29ba91dd0dbba981390db0689d45
Bastien <aublet.bastien@outlook.com>
2015-02-01 16:58:27 +0100

    New - Création de la solution pour le azure web site et l'azure web job.

---
+++ ConsoleApplication/StartStopVirtualMachine/Functions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace StartStopVirtualMachine
+{
+    public class Functions
+    {
+        public static void ProcessStartMessage([QueueTrigger("code-pushed")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+        }
+
+        public static void ProcessStopMessage([QueueTrigger("sources-built")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+        }
+    }
+}
