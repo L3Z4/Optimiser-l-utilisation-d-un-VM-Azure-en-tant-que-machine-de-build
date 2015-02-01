73639cc9d7855196418b666559b465edd07b2fcf
Bastien <aublet.bastien@outlook.com>
2015-02-01 18:50:43 +0100

    New - Ajout du service de gestion des VM azure.

--- ConsoleApplication/StartStopVirtualMachine/Functions.cs
+++ ConsoleApplication/StartStopVirtualMachine/Functions.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Management.Compute.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace StartStopVirtualMachine
 {
@@ -13,11 +17,15 @@ namespace StartStopVirtualMachine
         public static void ProcessStartMessage([QueueTrigger("code-pushed")] string message, TextWriter log)
         {
             log.WriteLine(message);
+            new VirtualMachineService().Start().Wait();
+            log.WriteLine("Virtual machine restarting...");
         }
 
         public static void ProcessStopMessage([QueueTrigger("sources-built")] string message, TextWriter log)
         {
             log.WriteLine(message);
+            new VirtualMachineService().Stop().Wait();
+            log.WriteLine("Virtual machine restarted.");
         }
     }
 }
---
+++ ConsoleApplication/StartStopVirtualMachine/VirtualMachineService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management.Compute.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartStopVirtualMachine
+{
+    public class VirtualMachineService
+    {
+        private ComputeManagementClient client;
+        private string serviceName;
+        private string deploymentName;
+        private string virtualMachineName;
+
+        public VirtualMachineService()
+        {
+            serviceName = System.Configuration.ConfigurationManager.AppSettings["serviceName"];
+            deploymentName = System.Configuration.ConfigurationManager.AppSettings["deploymentName"];
+            virtualMachineName = System.Configuration.ConfigurationManager.AppSettings["virtualMachineName"];
+
+            var subscriptionId = System.Configuration.ConfigurationManager.AppSettings["subscriptionId"];
+            var certificate = System.Configuration.ConfigurationManager.AppSettings["base64EncodedCertificate"];
+            client = new ComputeManagementClient(GetCredentials(subscriptionId, certificate));
+        }
+
+        public async Task Stop()
+        {
+            var operationStatusResponse = client.VirtualMachines.Shutdown(serviceName, deploymentName, virtualMachineName, new VirtualMachineShutdownParameters());
+        }
+
+        public async Task Start()
+        {
+            var operationStatusResponse = client.VirtualMachines.Start(serviceName, deploymentName, virtualMachineName);
+        }
+        private static SubscriptionCloudCredentials GetCredentials(string subscriptionId, string base64EncodedCertificate)
+        {
+            return new CertificateCloudCredentials(subscriptionId, new X509Certificate2(Convert.FromBase64String(base64EncodedCertificate)));
+        }
+    }
+}
